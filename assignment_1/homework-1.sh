793993a5b877e8d158dec4cf428b4917aea297bf
hezht3 <zhe33@jhu.edu>
2022-04-25 18:50:38 -0400

    correct sample prevalence

--- assignment_1/homework-1.sh
+++ assignment_1/homework-1.sh
@@ -15,11 +15,11 @@ head ./data/data_SCZ.txt     # schizophrenia
 # 3. Allele 2 (non-effect allele) -> A2
 # 4. Sample size -> N
 # 5. A P-value -> pval
-# 6. A signed summary statistic -> beta, z
+# 6. A signed summary statistic -> z
 python ./ldsc/munge_sumstats.py -h   # check munge_sumstats.py options
 python ./ldsc/munge_sumstats.py --sumstats ./data/data_BC17.txt --out ./clean/bc       # brest cancer
 python ./ldsc/munge_sumstats.py --sumstats ./data/data_BMI18.txt --ignore beta --out ./clean/bmi     # body mass index
-python ./ldsc/munge_sumstats.py --sumstats ./data/data_SCZ.txt --ignore beta --out ./clean/scz
+python ./ldsc/munge_sumstats.py --sumstats ./data/data_SCZ.txt --ignore beta --out ./clean/scz     # schizophrenia
 
 # Estimating Heritability
 python ./ldsc/ldsc.py --h2 ./clean/bc.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/bc_h2
@@ -27,5 +27,8 @@ python ./ldsc/ldsc.py --h2 ./clean/bmi.sumstats.gz --ref-ld-chr ./data/eur_w_ld_
 python ./ldsc/ldsc.py --h2 ./clean/scz.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/scz_h2
 
 # Conversion to Liability Scale
-python ./ldsc/ldsc.py --h2 ./clean/bc.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/bc_h2_lia --samp-prev 0.5 --pop-prev 0.0026
-python ./ldsc/ldsc.py --h2 ./clean/scz.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/scz_h2_lia --samp-prev 0.5 --pop-prev 0.0032
+## Breast cancer sample prevalence: 106571/(106571 + 95762) = 0.5267109
+python ./ldsc/ldsc.py --h2 ./clean/bc.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/bc_h2_lia --samp-prev 0.5267109 --pop-prev 0.0026     # total population
+python ./ldsc/ldsc.py --h2 ./clean/bc.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/bc_h2_lia_female --samp-prev 0.5267109 --pop-prev 0.0051     # female
+## Schizophrenia sample prevalence: 35476/(35476 + 46839) = 0.4309786
+python ./ldsc/ldsc.py --h2 ./clean/scz.sumstats.gz --ref-ld-chr ./data/eur_w_ld_chr/ --w-ld-chr ./data/eur_w_ld_chr/ --out ./clean/scz_h2_lia --samp-prev 0.4309786 --pop-prev 0.0032
